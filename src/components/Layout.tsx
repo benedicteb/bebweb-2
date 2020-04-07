9afc60374c0c822ac0ae4bfdb68fd66ae870e3f5
Benedicte Emilie Brækken <b@brkn.io>
2020-04-07 17:14:10 +0200

    Revert "Show empty page until fonts loaded"
    It's not good gatsby practice to use document.
    
    This reverts commit 6f5d70089fa27a14a44095fa6bfe321256970d23.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 
 import Header from "./Header";
@@ -6,12 +6,6 @@ import Header from "./Header";
 import "./Layout.css";
 
 const Layout: React.FC = ({ children }) => {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  document.fonts.ready.then(() => {
-    setFontsLoaded(true);
-  });
-
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -21,10 +15,6 @@ const Layout: React.FC = ({ children }) => {
       }
     }
   `);
-
-  if (!fontsLoaded) {
-    return null;
-  }
 
   return (
     <>
