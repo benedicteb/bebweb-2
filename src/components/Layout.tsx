6f5d70089fa27a14a44095fa6bfe321256970d23
Benedicte Emilie Brækken <b@brkn.io>
2020-04-07 17:01:29 +0200

    Show empty page until fonts loaded

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { graphql, useStaticQuery } from "gatsby";
 
 import Header from "./Header";
@@ -6,6 +6,12 @@ import Header from "./Header";
 import "./Layout.css";
 
 const Layout: React.FC = ({ children }) => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  document.fonts.ready.then(() => {
+    setFontsLoaded(true);
+  });
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -15,6 +21,10 @@ const Layout: React.FC = ({ children }) => {
       }
     }
   `);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <>
